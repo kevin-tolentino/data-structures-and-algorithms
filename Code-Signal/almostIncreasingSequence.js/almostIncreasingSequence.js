e27aa08fbc739743b599cca4bf5e25ab7101a13f
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-20 21:53:50 -0700

    added almostIncreasingSequence problem (still finishing)

---
+++ Code-Signal/almostIncreasingSequence.js/almostIncreasingSequence.js
@@ -0,0 +1,62 @@
+//notes
+//run a loop once to check if the sequence is increasing
+//define a counter that will increment if the current nummber is greater than the previous number in sequence
+// if counter is greater than 1, return false, else return true
+//no repeats
+
+
+
+//pseudocode
+//1. make a placeholder that will act as our counter (done)
+//2. go through the array and compare current integer with integer right before it; start at second index (done)
+//2a. if previous number is greater than current, increment counter by 1
+//3. check for duplicate numbers
+//4.. if counter is greater than 1 and duplicateCheck is false, return false, else return true
+
+function almostIncreasingSequence(sequence) {
+  let counter = 0;
+  let duplicateCheck = false;
+  let minimumCounter = 0;
+  let currentMin = sequence[0];
+
+
+
+  for (let i = 1; i < sequence.length; i++) {
+
+    if (sequence[i] < sequence[i - 1]) {
+      counter = counter + 1;
+    }
+  }
+  console.log(counter)
+
+  let currentValue;
+
+  for (let i = 0; i < sequence.length; i++) {
+    currentValue = sequence[i]
+    for (let j = i + 1; j < sequence.length; j++) {
+      if (currentValue === sequence[j]) {
+        duplicateCheck = true
+      }
+    }
+  }
+
+  if (counter > 1) {
+    for (let i = 0; i < sequence.length - 1; i++) {
+
+      if (sequence[i] > sequence[i + 1]) {
+        minimumCounter = minimumCounter + 1
+      }
+    }
+
+  }
+  console.log(minimumCounter)
+
+  console.log(duplicateCheck)
+
+  if (counter > 1 || duplicateCheck || minimumCounter > 0) {
+    return false
+  } else {
+    return true
+  }
+
+}
--- Code-Signal/makeArrayConsecutive2/makeArrayConsecutive2.js
+++ Code-Signal/makeArrayConsecutive2/makeArrayConsecutive2.js
@@ -1,12 +1,46 @@
-//make placeholder for area set equal to 1 init.
-//iterate through the input n
-// get area by adding existing area plus i multiplied by 4 subtracted by 4 to account for duplicate sides
-//return area when iteration is done
+//notes
+//use two variables for min and max
+//go through array and compare numbers for min and max against each value in the array
+//go through array again to tally about many values are in the range
+//subtract that by the difference of min and max
 
-function shapeArea(n) {
-  let area = 1
-  for (let i = 1; i <= n; i++) {
-    area = area + i * 4 - 4;
+
+
+//pseudocode
+//prepare two placeholders for the minimum and maximum size values of the statues (done)
+//iterate through the group of statues to determine the minimum and maximum values (done)
+// go through group again to tally how many statue sizes are within the range (done)
+//subtract that tally by the difference of min and max to get how many statues are left
+
+function makeArrayConsecutive2(statues) {
+  if (statues.length === 1) return 0
+
+  let min = statues[0];
+  let max = statues[0];
+
+  for (let i = 0; i < statues.length; i++) {
+    let currentValue = statues[i]
+
+    if (min >= currentValue) {
+      min = currentValue
+    }
+    if (max <= currentValue) {
+      max = currentValue
+    }
   }
-  return area
+
+  let tally = 0;
+
+  for (let i = 0; i < statues.length; i++) {
+    if (statues[i] > min && statues[i] < max) {
+      tally++
+    }
+  }
+  console.log('tally', tally)
+
+  const statuesNeeded = (max - min) - tally - 1
+  console.log('statues', statuesNeeded)
+
+  return statuesNeeded
+
 }
