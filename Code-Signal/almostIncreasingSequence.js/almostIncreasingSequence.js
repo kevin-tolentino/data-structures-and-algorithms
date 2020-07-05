ae9cd53948dfb2c11316c13f572c985232134a51
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-07-04 22:57:55 -0700

    completed almostIncreasingSequence challenge

--- Code-Signal/almostIncreasingSequence.js/almostIncreasingSequence.js
+++ Code-Signal/almostIncreasingSequence.js/almostIncreasingSequence.js
@@ -11,60 +11,21 @@
 //pseudocode
 //1. make a placeholder that will act as our counter (done)
 //2. go through the array and compare current integer with integer right before it; start at second index (done)
-//2a. if previous number is greater than current, increment counter by 1
+    //2a. if previous number is greater than or equal current, increment counter by 1
 //3. check for duplicate numbers
 //4.. if counter is greater than 1 and duplicateCheck is false, return false, else return true
 
 function almostIncreasingSequence(sequence) {
-  let counter = 0;
-  let duplicateCheck = false;
-  let minimumCounter = 0;
-  let currentMin = sequence[0];
+let counter = 0;
 
+    for (let i=0; i < sequence.length; i++) {
+      if (sequence[i] <= sequence[i-1]) {
+        counter++;
+        if (counter > 1) return false;
+        if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) return false;
+      }
 
-
-  for (let i = 0; i < sequence.length - 1; i++) {
-
-    if (sequence[i] > sequence[i + 1]) {
-      counter = counter + 1;
     }
-  }
-  console.log(counter)
-
-  if (counter < 2) {
-    return true
-  } else {
-    return false
-  }
-
-  // let currentValue;
-
-  // for (let i = 0; i < sequence.length; i++ ){
-  //     currentValue = sequence[i]
-  //     for (let j = i + 1; j < sequence.length; j++){
-  //         if (currentValue === sequence[j]){
-  //             duplicateCheck = true
-  //         }
-  //     }
-  // }
-
-  // if (counter > 1){
-  // for (let i = 0; i <  sequence.length-1; i++){
-
-  //   if (sequence[i] > sequence[i + 1]){
-  //         minimumCounter = minimumCounter + 1
-  //     }
-  // }
-
-  // }
-  // console.log(minimumCounter)
-
-  // console.log(duplicateCheck)
-
-  // if (counter > 1 || duplicateCheck || minimumCounter > 0) {
-  //     return false
-  // } else {
-  //     return true
-  // }
+    return true;
 
 }
