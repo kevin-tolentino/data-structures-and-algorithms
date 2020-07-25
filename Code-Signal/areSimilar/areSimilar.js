d069ab6b9ad9a79ef52d3179200489bdef428e04
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-07-24 17:48:01 -0700

    working on areSimilar solution

---
+++ Code-Signal/areSimilar/areSimilar.js
@@ -0,0 +1,12 @@
+//Notes
+// compare the first array with the second array at the same time
+// stage a comparison with logical operators
+//if not the same, return false
+
+
+//Pseudocode
+
+
+function areSimilar(a, b) {
+
+}
