5530d0452d45518fd1cbbeca104d0ca7f45b641b
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-07-25 23:52:20 -0700

    completed first pass of pseudocode for challenge and starting the coding section

--- Code-Signal/areSimilar/areSimilar.js
+++ Code-Signal/areSimilar/areSimilar.js
@@ -5,8 +5,15 @@
 
 
 //Pseudocode
-
+// 1. create a counter that will increment if not similar
+// 2. go through array b, compare with array a.
+// // 2a. if array index value for both arrays match, then continue
+//2b. if not, then check if swapping from left index or right index will allow it to work
 
 function areSimilar(a, b) {
+  let notSimilarCounter = 0;
+  for (let i = 0; i < b.length; i++) {
+    //stopped here
+  }
 
 }
